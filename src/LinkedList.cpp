71053e9ffc1f9b7b06e15880cfefc6ff810895b8
amadoaguad1 <amadoaguad1@kenyon.edu>
2025-10-16 17:52:59 -0400

    submit

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -2,25 +2,50 @@
 #include <sstream>
 
 LinkedList::LinkedList() : head_(nullptr) {}
+
 LinkedList::~LinkedList() {
     Node* p = head_;
-    while (p) { Node* n = p->next; delete p; p = n; }
-}
-void LinkedList::push_front(int v) { head_ = new Node(v, head_); }
-void LinkedList::remove_evens() {
-    // TODO: implement
-}
-std::string LinkedList::to_string() const {
-    std::ostringstream oss;
-    oss << "[";
-    Node* p = head_;
-    bool first = true;
     while (p) {
-        if (!first) oss << ", ";
-        first = false;
-        oss << p->value;
-        p = p->next;
+        Node* nextNode = p->next;
+        delete p;
+        p = nextNode;
     }
-    oss << "]";
-    return oss.str();
+}
+
+void LinkedList::push_front(int v) {
+    head_ = new Node(v, head_);
+}
+
+void LinkedList::remove_evens() {
+    while (head_ && head_->value % 2 == 0) {
+        Node* temp = head_;
+        head_ = head_->next;
+        delete temp;
+    }
+
+    Node* node = head_;
+    while (node && node->next) {
+        if (node->next->value % 2 == 0) {
+            Node* toRemove = node->next;
+            node->next = toRemove->next;
+            delete toRemove;
+        } else {
+            node = node->next;
+        }
+    }
+}
+
+std::string LinkedList::to_string() const {
+    std::ostringstream out;
+    out << "[";
+    Node* n = head_;
+    bool first = true;
+    while (n) {
+        if (!first) out << ", ";
+        first = false;
+        out << n->value;
+        n = n->next;
+    }
+    out << "]";
+    return out.str();
 }
